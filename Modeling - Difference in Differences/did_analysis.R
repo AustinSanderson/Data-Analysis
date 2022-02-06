d1d7a1fe3c6f1b62cb912b9b57afe12ae1e7aee7
Austin <j.austin.sanderson@gmail.com>
2022-02-06 17:46:49 -0500

    removed extraneous files

--- Modeling - Difference in Differences/did_analysis.R
+++ Modeling - Difference in Differences/did_analysis.R
@@ -3,32 +3,45 @@ library(tidyverse)
 library(readxl)
 
 # Reading in the data
-# Note: Testing two time periods: (2007 - 2020) and (2009 - 2018) - file names reflected as such 
+# Note: Testing time periods: (2007 - 2020) and (2009 - 2018) - file names reflected as such 
 # 
 # Laptop file path
- dta <- read_excel("/Users/roadie/Dropbox/Grad School/Independent Research/Homelessness/Projects/Thesis/Working Data/(2007 - 2020) Agg Master Minimal + Treatment Groups + Time-invariant Controls.xlsx")
+# dta <- read_excel("/Users/roadie/Dropbox/Grad School/Independent Research/Homelessness/Projects/Thesis/Working Data/DUP (2007 - 2020) Agg Master Minimal + Treatment Groups + Time-invariant Controls.xlsx")
 #
 # Desktop file path
-# dta <- read_excel("/Users/roygbiv/Dropbox/Grad School/Independent Research/Homelessness/Projects/Thesis/Working Data/(2007 - 2020) Agg Master Minimal + Treatment Groups + Time-invariant Controls.xlsx")
+ dta <- read_excel("/Users/roygbiv/Dropbox/Grad School/Independent Research/Homelessness/Projects/Thesis/Working Data/(2007 - 2020) Agg Master Minimal + Treatment Groups + Time-invariant Controls.xlsx")
 
 # Quick summary count of first_treated group
 table(dta$first_treated)
 
-# converting the id variable to numeric
-dta$CoC_number <- as.numeric(as.character(dta$CoC_number))
+# converted the id variable to numeric
+# dta$CoC_number <- as.numeric(as.character(dta$CoC_number))
+
+# DIDparams(yname = "overall_homeless_per10k",
+#   tname = "year",
+#   gname = "first_treated",
+#   idname = "CoC_number",
+#   xformla=~pop_dens + avg_low_temp_cnty_avg + total_jail_pop_per10k,
+#   # Commenting out additional controls for testing
+#   # all_ages_in_poverty_percent_cnty_avg + avg_low_temp_cnty_avg
+#   # total_jail_pop_per10k + all_ages_in_poverty_percent_cnty_avg + shelter_beds_per10k 
+#   # total_jail_adm_per10k + total_jail_dis_per10k + total_prison_pop_per10k + total_prison_adm_per10k + med_rent + median_household_income,
+#   data = dta,
+#   control_group = "nevertreated",
+#   clustervars = "state",
+#   #allow_unbalanced_panel = FALSE
+#   )
 
 # Checking the parallel trends assumption
 if (FALSE) {
-data(dta)
-pre.test <- conditional_did_pretest(yname="overall_homeless_per10k",
-                                    tname="year",
-                                    idname="CoC_number",
-                                    gname="first_treated",
-                                    xformla=~avg_low_temp_cnty_avg + total_jail_pop_per10k,
-                                    # Commenting out additional controls for testing
-                                    # total_jail_adm_per10k + total_jail_dis_per10k + total_prison_pop_per10k + total_prison_adm_per10k + med_rent+median_household_income,
-                                    data=dta)
-summary(pre.test)
+  data(dta)
+  pre.test <- conditional_did_pretest(yname="overall_homeless_per10k",
+                                      tname="year",
+                                      idname="CoC_number",
+                                      gname="first_treated",
+                                      xformla=~ avg_low_temp_cnty_avg + total_jail_pop_per10k + med_rent + shelter_beds_per10k,
+                                      data=dta)
+  summary(pre.test)
 }
 
 # Estimate group-time average treatment effects
@@ -36,15 +49,15 @@ out1 <- att_gt(yname = "overall_homeless_per10k",
                         tname = "year",
                         gname = "first_treated",
                         idname = "CoC_number",
-                        xformla=~avg_low_temp_cnty_avg + total_jail_pop_per10k,
+                        # Group 2019 - Chicago - becomes insignificant when controlling for shelter_beds
+                        xformla=~ avg_low_temp_cnty_avg + total_jail_pop_per10k + med_rent + shelter_beds_per10k,
                         # Commenting out additional controls for testing
-                        # pop_dens is causing issues with group 2012; check data
                         # all_ages_in_poverty_percent_cnty_avg + avg_low_temp_cnty_avg
                         # total_jail_pop_per10k + all_ages_in_poverty_percent_cnty_avg + shelter_beds_per10k 
-                        # total_jail_adm_per10k + total_jail_dis_per10k + total_prison_pop_per10k + total_prison_adm_per10k + med_rent + median_household_income,
+                        # total_jail_adm_per10k + total_jail_dis_per10k + med_rent,
                         data = dta,
                         control_group = "nevertreated",
-                        #allow_unbalanced_panel = FALSE, 
+                        #allow_unbalanced_panel = FALSE", 
 )
 
 # summarize the results
@@ -58,7 +71,7 @@ aggout <- aggte(
   # balance_e = NULL,
   # min_e = -Inf,
   # max_e = Inf,
-  na.rm = TRUE,
+  # na.rm = TRUE,
   # bstrap = NULL,
   # biters = NULL,
   # cband = NULL,
@@ -70,21 +83,13 @@ aggout <- aggte(
 summary(aggout)
 ggdid(aggout)
 
-# Storing DID parameters
+# Unused DID parameters
 # DIDparams(
-#   yname,
-#   tname,
-#   idname = NULL,
-#   gname,
-#   xformla = NULL,
-#   data,
-#   control_group,
 #   anticipation = 0,
 #   weightsname = NULL,
 #   alp = 0.05,
 #   bstrap = TRUE,
 #   biters = 1000,
-#   clustervars = NULL,
 #   cband = TRUE,
 #   print_details = TRUE,
 #   pl = FALSE,
@@ -99,31 +104,6 @@ ggdid(aggout)
 #   tlist = NULL,
 #   glist = NULL,
 #   call = NULL
-# )
-
-
-
-# # More details on pretest parameters below
-# conditional_did_pretest(
-#   yname,
-#   tname,
-#   idname = NULL,
-#   gname,
-#   xformla = NULL,
-#   data,
-#   panel = TRUE,
-#   allow_unbalanced_panel = FALSE,
-#   control_group = c("nevertreated", "notyettreated"),
-#   weightsname = NULL,
-#   alp = 0.05,
-#   bstrap = TRUE,
-#   cband = TRUE,
-#   biters = 1000,
-#   clustervars = NULL,
-#   est_method = "ipw",
-#   print_details = FALSE,
-#   pl = FALSE,
-#   cores = 1
 # )
 
 
